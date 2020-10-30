68aa8ffe8e344dad677b47ff4c5bfbd00562bd44
Felipe Arteaga <lufarteagada@unal.edu.co>
2020-10-30 17:01:55 -0500

    cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function allows me to store the square matrix
+## Input : Invertible matrix(nxn)
+## Output : List 
 
 makeCacheMatrix <- function(x = matrix()) {
         invMatrix <- NULL
@@ -19,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##  Calculate the inverse of the matrix
+## Input : list of makeCacheMatrix
+## Output : Invertible matrix(nxn) 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
